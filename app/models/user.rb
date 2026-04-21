8a269168f67d94bc4ae2dbd7dd1b1f5b47d1b798
kenyoshizawa <harborseal2521@gmail.com>
2026-04-21 13:33:16 +0000

    [Add] ユーザー登録の際、社員用メールアドレスのドメインのみ許可する #7 やったこと： ・社員用メールアドレスのドメインを環境変数（.env）に格納 ・カスタムバリデーターを作成し、指定ドメイン以外はエラーを返す処理を追加 ・Userモデルのemailに上記のカスタムバリデーションを適用
    理由：
    社員以外の人間がユーザー登録できないようにするため

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true, uniqueness: { case_insensitive: true }, allowed_domain: true, allow_blank: true
 end
