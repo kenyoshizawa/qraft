3002537c4bc83a7e9bd665b1b44aef916d18d53d
kenyoshizawa <harborseal2521@gmail.com>
2026-04-23 11:35:29 +0000

    [Add] Userモデルname属性、name_kana属性にバリデーションを付与 #9 やったこと： - name・name_kana属性に最大20字・氏名の間に半角スペースを必須とするバリデーションを追加 - name_kanaは全角カタカナであるバリデーションも追加 - アカウント編集画面のname、name_kanaフィールドで、複数のエラーを表示できるよう修正
    理由：
    本アプリを使用するユーザーは日本人を想定しているため、名前は20字以内としている。
    社員名は見積書にそのまま使うため、氏名の間に半角スペースを入れることを必須としている。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,22 @@ class User < ApplicationRecord
 
   enum role: { general: 0, admin: 1, retired: 2 }
 
-  validates :email, presence: true, uniqueness: { case_insensitive: true }, allowed_domain: true, allow_blank: true
+  VALID_NAME_REGEX = /\A[^\p{blank}]+ [^\p{blank}]+\z/
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー]+(?: [ァ-ヶー]+)*\z/
+
+  validates :email, uniqueness: { case_sensitive: false }, allowed_domain: true
+
+  validates :name,
+    length: { maximum: 20 },
+    format: { with: VALID_NAME_REGEX, message: "は氏名の間に半角スペースを入れてください" },
+    allow_blank: true
+  validates :name_kana,
+    length: { maximum: 20 },
+    format: { with: VALID_NAME_REGEX, message: "は氏名の間に半角スペースを入れてください" },
+    allow_blank: true
+  validates :name_kana,
+    format: { with: VALID_NAME_KANA_REGEX, message: "は全角カタカナで入力してください" },
+    allow_blank: true
 
   def active_for_authentication?
     super && !retired?
