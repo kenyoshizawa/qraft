5c734bf9863613d000f4fcdb456438cdf6c09733
kenyoshizawa <harborseal2521@gmail.com>
2026-04-23 07:54:17 +0000

    [Add] アカウント無効化機能の追加 #7 やったこと： アカウント編集画面にアカウント無効化に関する説明文とボタンを追加 registrations_controller に deactivate アクションを追加 deactivate アクションに対応するルーティングを追加 無効化アカウントのログインを禁止する active_for_authentication? を追加 ログイン失敗時のエラー種別を返す inactive_message を追加 無効化アカウントのログインエラーメッセージを翻訳ファイルに追加
    理由：
    退職者のレコードを削除せず、ログインのみ禁止することで、見積作成者などの参照履歴を保持するため

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  enum role: { general: 0, admin: 1, retired: 2 }
+
   validates :email, presence: true, uniqueness: { case_insensitive: true }, allowed_domain: true, allow_blank: true
+
+  def active_for_authentication?
+    super && !retired?
+  end
+
+  def inactive_message
+    retired? ? :retired_account : super
+  end
 end
