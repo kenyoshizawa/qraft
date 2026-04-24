66b5e3bad288a26953fbc1f10aaee1cd836cad3f
kenyoshizawa <harborseal2521@gmail.com>
2026-04-24 14:12:13 +0900

    Revert "[Add] ユーザー登録の際、社員用メールアドレスのドメインのみ許可する #7" やったこと： - email属性の case_insensitive: true を case_sensitive: false に変更 - email属性の presence: true を削除 - email属性の allow_blank: true を削除し、カスタムバリデーション   allowed_domain に return if value.blank? を追記
    理由：
    - タイプミス
    - devise がデフォルトで presence: true を設定しているため
    - presence: true と allow_blank: true が存在し矛盾した状態であったため
    This reverts commit 8a269168f67d94bc4ae2dbd7dd1b1f5b47d1b798.

--- app/validators/allowed_domain_validator.rb
+++ app/validators/allowed_domain_validator.rb
@@ -1,6 +1,6 @@
-# app/validators/allowed_domain_validator.rb
 class AllowedDomainValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
+    return if value.blank?
     # 環境変数からドメインを取得
     allowed_domains = ENV["ALLOWED_DOMAIN"].to_s.split(",")
 
