a04662ca44256f0c538acc456584e9a258742d94
kenyoshizawa <harborseal2521@gmail.com>
2026-04-30 07:47:19 +0000

    [Add] Users テーブルに company_id 属性追加 #15 やったこと： - rails g migration AddCompanyIdToUsers company:references 実行 - company_id 属性に外部キー制約を付与 - company_id 属性から NOT NULL 制約を削除
    理由：
    CompanyモデルとUserモデルに 1:N のアソシエーションを設定するため
    NOT NULL 制約の削除は、招待メール経由しなくてもアカウント登録できるようにするため

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_28_101609) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_30_073620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,10 +45,14 @@ ActiveRecord::Schema[7.2].define(version: 2026_04_28_101609) do
     t.string "invited_by_type"
     t.bigint "invited_by_id"
     t.integer "invitations_count", default: 0
+    t.bigint "company_id"
+    t.index ["company_id"], name: "index_users_on_company_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
     t.index ["invited_by_type", "invited_by_id"], name: "index_users_on_invited_by"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "users", "companies"
 end
