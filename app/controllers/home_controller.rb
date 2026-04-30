f5edc2a5138074972271369cdb200e5994032d1e
kenyoshizawa <harborseal2521@gmail.com>
2026-04-30 13:12:53 +0000

    [Add] 自社情報登録状態に基づくナビゲーション表示制御 #16 やったこと： - ログイン後のリダイレクト先ロジック   - after_sign_in_path_for メソッドオーバーライド     - ユーザーが自社情報に関連づけられている場合 ⇨ root_path     - ユーザーが管理者の場合 ⇨ new_company_path(自社情報を登録するページ)にリダイレクト     - 上記以外の場合 ⇨ invitation_required_path(管理者から招待を受けるよう誘導するページ)にリダイレクト - アクセスガードのロジック   - company_registered! メソッド定義     - ログイン済みユーザーで、自社情報が登録されていない場合：         - 管理者ユーザー ⇨ new_company_path(自社情報を登録するページ)にリダイレクト         - 一般ユーザー ⇨ invitation_required_path(管理者から招待を受けるよう誘導するページ)にリダイレクト   - app/controllers/home_controller.rb 編集     - ログイン中のみ before_action :company_registered! 実行   - app/controllers/users/invitations_controller.rb 編集     - 全てのアクションで before_action :company_registered! 実行 - navbarの制御   - app/views/layouts/_navbar.html.erb 作成     - 「ログイン済み」かつ「アカウントが自社情報に関連付けられている」場合のみナビゲーションリンク表示：         - 一般ユーザー ⇨ Home         - 管理者ユーザー ⇨ Home、招待   - レイアウトにパーシャルをレンダリング     - app/views/layouts/application.html.erb に app/views/layouts/_navbar.html.erb 挿入     - app/views/layouts/application.html.erb のデザイン修正     - app/views/layouts/_header.html.erb のデザイン修正
    理由：
    - ログイン後のリダイレクト先ロジック
      - adminユーザー（company作成済み） ⇨ root_path にリダイレクト
      - adminユーザー（company作成前） ⇨ new_company_path にリダイレクト
      - generalユーザー（company紐付け後） ⇨ root_path にリダイレクト
      - generalユーザー（company紐付け前） ⇨ invitation_required_path にリダイレクト
    - アクセスガードのロジック
      - adminユーザー（company作成済み） ⇨ リクエストが成功する
      - adminユーザー（company作成前） ⇨ new_company_path にリダイレクト
      - generalユーザー（company紐付け後） ⇨ リクエストが成功する
      - generalユーザー（company紐付け前） ⇨ invitation_required_path にリダイレクト
    - navbarの制御
      - 管理者ユーザーに自社情報の登録、一般ユーザーを招待する権限を与えるため

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :company_registered!, if: :user_signed_in?
 
   def top
   end
--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,2 +1,3 @@
 class Users::InvitationsController < Devise::InvitationsController
+  before_action :company_registered!
 end
