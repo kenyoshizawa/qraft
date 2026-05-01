254fb0c154bd1973abe825aa235e90852ca35680
kenyoshizawa <harborseal2521@gmail.com>
2026-05-01 02:01:54 +0000

    Revert "configure_permitted_parameters メソッドを public から protected に変更" #18
    This reverts commit bb7b7d3d6b54d8ef98853d4b0339d7a69ff2b51c.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:account_update, keys: [ :name, :name_kana, :phone ])
   end
