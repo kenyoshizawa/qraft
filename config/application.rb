772ebce4a0a3bd9f8e1bc974756e66a2674d8a9e
kenyoshizawa <harborseal2521@gmail.com>
2026-05-07 05:39:48 +0000

    feat: 翻訳ファイル読み込みパスを設定 #20

--- config/application.rb
+++ config/application.rb
@@ -47,6 +47,7 @@ module Qraft
     config.time_zone = "Tokyo"
     config.active_record.default_timezone = :utc
 
+    config.i18n.load_path += Dir[Rails.root.join("config/locales/**/*.{rb,yml}")]
     config.i18n.available_locales = %i[ja]
     config.i18n.default_locale = :ja
 
