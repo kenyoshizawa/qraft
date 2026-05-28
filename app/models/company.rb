644becb5b1d373c408753129c454ee3ce386ff28
kenyoshizawa <harborseal2521@gmail.com>
2026-05-28 03:56:34 +0000

    fix: PostalCodeFormatValidatorの作成に伴い、Companyモデルのバリデーションを修正 #26

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,14 +1,8 @@
 class Company < ApplicationRecord
   has_many :users, dependent: :restrict_with_error
 
-  VALID_POSTAL_CODE_REGEX = /\A\d{7}\z/
-
   validates :name, presence: true
-  validates :postal_code, presence: true,
-    format: {
-      with: VALID_POSTAL_CODE_REGEX,
-      message: "は半角数字（ハイフンなし）で入力してください"
-    }
+  validates :postal_code, presence: true, postal_code_format: true
   validates :address, presence: true
   validates :phone, presence: true, phone: { possible: true, countries: :jp }, phone_format: true
   validates :fax, presence: true, phone: { possible: true, countries: :jp }, phone_format: true
