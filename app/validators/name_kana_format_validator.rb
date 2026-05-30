b38a2815eb6de908a147490fe1388b38e8a3bb75
kenyoshizawa <harborseal2521@gmail.com>
2026-05-30 12:39:21 +0000

    feat: NameKanaFormatValidatorの作成 #32

---
+++ app/validators/name_kana_format_validator.rb
@@ -0,0 +1,14 @@
+class NameKanaFormatValidator < ActiveModel::EachValidator
+  VALID_NAME_REGEX = /\A[^\p{blank}]+ [^\p{blank}]+\z/
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー]+(?: [ァ-ヶー]+)*\z/
+
+  def validate_each(record, attribute, value)
+    return if value.blank?
+
+    if !value.match?(VALID_NAME_REGEX)
+      record.errors.add(attribute, :invalid, message: "は氏名の間に半角スペースを入れてください")
+    elsif !value.match?(VALID_NAME_KANA_REGEX)
+      record.errors.add(attribute, :invalid, message: "は全角カタカナで入力してください")
+    end
+  end
+end
