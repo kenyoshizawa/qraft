1cd63c13d94378a7d0855f25dad80c90d38b5730
kenyoshizawa <harborseal2521@gmail.com>
2026-06-05 08:40:00 +0000

    test: PagesController#invitation_requiredでrequire_company!の実行をskip #41 理由： Company未登録のgeneralユーザーがアクセスできるようにするため

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :require_company!, only: %i[ invitation_required ]
+
   def invitation_required
     redirect_to root_path if current_user&.company.present?
   end
