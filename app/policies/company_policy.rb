c636197ed7d9e2675d0f0d23f7cde4f313cd039a
kenyoshizawa <harborseal2521@gmail.com>
2026-06-11 11:53:42 +0000

    feat: CompanyPolicyを作成 #47 やったこと： - rails generate pundit:policy company 実行 - 各アクションの権限ルール記述   - show：     - ユーザーがその会社に所属していれば閲覧可能   - new, create：     - 管理者かつ、まだ会社に所属していない場合のみ作成可能   - edit, update：     - 管理者かつ、自分の所属する会社のみ編集可能
    理由：
    以下のようなユーザーがURLに直接入力したときにアクセスできないようにするため
    - generalユーザー（companyあり）が GET /companies/new にアクセスしてしまう
    - generalユーザー（companyあり）が GET /companies/:id/edit にアクセスしてしまう

---
+++ app/policies/company_policy.rb
@@ -0,0 +1,34 @@
+class CompanyPolicy < ApplicationPolicy
+  # NOTE: Up to Pundit v2.3.1, the inheritance was declared as
+  # `Scope < Scope` rather than `Scope < ApplicationPolicy::Scope`.
+  # In most cases the behavior will be identical, but if updating existing
+  # code, beware of possible changes to the ancestors:
+  # https://gist.github.com/Burgestrand/4b4bc22f31c8a95c425fc0e30d7ef1f5
+
+  def show?
+    user.company_id == record.id
+  end
+
+  def new?
+    user.admin? && user.company_id.nil?
+  end
+
+  def create?
+    new?
+  end
+
+  def edit?
+    user.admin? && user.company_id == record.id
+  end
+
+  def update?
+    edit?
+  end
+
+  class Scope < ApplicationPolicy::Scope
+    # NOTE: Be explicit about which records you allow access to!
+    # def resolve
+    #   scope.all
+    # end
+  end
+end
