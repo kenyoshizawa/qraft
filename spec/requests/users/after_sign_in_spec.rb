bdc247c0913f2c4d8133944252def91d22bd22ff
kenyoshizawa <harborseal2521@gmail.com>
2026-06-15 12:31:00 +0000

    chore: テストケースのグループ分け順を変更 #49

--- spec/requests/users/after_sign_in_spec.rb
+++ spec/requests/users/after_sign_in_spec.rb
@@ -2,8 +2,8 @@ require "rails_helper"
 
 RSpec.describe "After sign in", type: :request do
   describe "POST /users/sign_in" do
-    context "adminユーザーの場合" do
-      context "company登録済みの場合" do
+    context "company登録済みの場合" do
+      context "adminユーザーの場合" do
         it "root_pathにリダイレクトされること" do
           admin = create(:user, :admin, :with_company)
           post user_session_path, params: { user: { email: admin.email, password: admin.password } }
@@ -11,25 +11,25 @@ RSpec.describe "After sign in", type: :request do
         end
       end
 
-      context "company未登録の場合" do
-        it "new_company_pathにリダイレクトされること" do
-          admin = create(:user, :admin, company: nil)
-          post user_session_path, params: { user: { email: admin.email, password: admin.password } }
-          expect(response).to redirect_to(new_company_path)
-        end
-      end
-    end
-
-    context "generalユーザーの場合" do
-      context "company登録済みの場合" do
+      context "generalユーザーの場合" do
         it "root_pathにリダイレクトされること" do
           general = create(:user, :general, :with_company)
           post user_session_path, params: { user: { email: general.email, password: general.password } }
           expect(response).to redirect_to(root_path)
         end
       end
+    end
 
-      context "company未登録の場合" do
+    context "company未登録の場合" do
+      context "adminユーザーの場合" do
+        it "new_company_pathにリダイレクトされること" do
+          admin = create(:user, :admin, company: nil)
+          post user_session_path, params: { user: { email: admin.email, password: admin.password } }
+          expect(response).to redirect_to(new_company_path)
+        end
+      end
+
+      context "generalユーザーの場合" do
         it "invitation_required_pathにリダイレクトされること" do
           general = create(:user, :general, company: nil)
           post user_session_path, params: { user: { email: general.email, password: general.password } }
