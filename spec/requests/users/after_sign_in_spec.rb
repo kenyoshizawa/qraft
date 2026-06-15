c5eebd69fafd0c92dc0900b7d65d7cd72888e0c2
kenyoshizawa <harborseal2521@gmail.com>
2026-06-15 12:32:14 +0000

    test: company未登録の場合のテストグループに、指定したフラッシュメッセージが表示されるか確認するテストを追加 #49

--- spec/requests/users/after_sign_in_spec.rb
+++ spec/requests/users/after_sign_in_spec.rb
@@ -27,6 +27,12 @@ RSpec.describe "After sign in", type: :request do
           post user_session_path, params: { user: { email: admin.email, password: admin.password } }
           expect(response).to redirect_to(new_company_path)
         end
+
+        it "指定したフラッシュメッセージが表示されること" do
+          admin = create(:user, :admin, company: nil)
+          post user_session_path, params: { user: { email: admin.email, password: admin.password } }
+          expect(flash[:alert]).to eq("自社情報を登録してください")
+        end
       end
 
       context "generalユーザーの場合" do
@@ -34,6 +40,12 @@ RSpec.describe "After sign in", type: :request do
           general = create(:user, :general, company: nil)
           post user_session_path, params: { user: { email: general.email, password: general.password } }
           expect(response).to redirect_to(invitation_required_path)
+        end
+
+        it "指定したフラッシュメッセージが表示されること" do
+          general = create(:user, :general, company: nil)
+          post user_session_path, params: { user: { email: general.email, password: general.password } }
+          expect(flash[:alert]).to eq("管理者ユーザーから招待メールを受け取ってください")
         end
       end
     end
