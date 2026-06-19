06712defaeb089225bc51b4f5aa7ba9e686da71a
kenyoshizawa <harborseal2521@gmail.com>
2026-06-19 03:35:15 +0000

    test: CompaniesController#showのテストケース作成 #51 やったこと： 以下の表に従って、ステータスコードとリダイレクト先が期待通りか検証するテストケースを作成した。
    | company | ユーザー種別 | 実行されるメソッド | ステータスコード | リダイレクト先 |
    |---|---|---|---|---|
    | 登録済み | admin | `user_not_authorized` | 302 | `root_path` |
    | 登録済み | general | `CompaniesController#show` | 200 | - |
    | 未登録 | admin | `user_not_authorized` | 302 | `new_company_path` |
    | 未登録 | general | `user_not_authorized` | 302 | `invitation_required_path` |

---
+++ spec/requests/companies_spec.rb
@@ -0,0 +1,59 @@
+require "rails_helper"
+
+RSpec.describe "Companies", type: :request do
+  describe "GET /companies/:id" do
+    context "ログイン済みの場合" do
+      context "company登録済みの場合" do
+        context "adminユーザーの場合" do
+          let(:user) { create(:user, :admin, :with_company) }
+
+          it "トップページにリダイレクトすること" do
+            sign_in user
+            get company_path(user.company)
+            expect(response).to redirect_to(root_path)
+          end
+        end
+
+        context "generalユーザーの場合" do
+          let(:user) { create(:user, :general, :with_company) }
+
+          it "HTTPステータス200を返すこと" do
+            sign_in user
+            get company_path(user.company)
+            expect(response).to have_http_status(:ok)
+          end
+
+          it "自社情報が取得できること" do
+            sign_in user
+            get company_path(user.company)
+            expect(response.body).to include(user.company.name)
+          end
+        end
+      end
+
+      context "company未登録の場合" do
+        let(:other_user) { create(:user, :admin, :with_company) }
+
+        context "adminユーザーの場合" do
+          let(:user) { create(:user, :admin, company: nil) }
+
+          it "自社情報登録ページにリダイレクトすること" do
+            sign_in user
+            get company_path(other_user.company)
+            expect(response).to redirect_to(new_company_path)
+          end
+        end
+
+        context "generalユーザーの場合" do
+          let(:user) { create(:user, :general, company: nil) }
+
+          it "招待待ちページにリダイレクトすること" do
+            sign_in user
+            get company_path(other_user.company)
+            expect(response).to redirect_to(invitation_required_path)
+          end
+        end
+      end
+    end
+  end
+end
