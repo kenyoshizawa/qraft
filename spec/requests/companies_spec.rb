d00af7fa76722d02a5121e638984772e146417d3
kenyoshizawa <harborseal2521@gmail.com>
2026-06-19 06:29:40 +0000

    test: CompaniesController#createのテストケース追加 #51 やったこと： 以下の表に従って、ステータスコードとリダイレクト先が期待通りか検証するテストケースを作成した。
    | company | ユーザー種別 | バリデーション | 実行されるメソッド | ステータスコード | リダイレクト先 |
    |---|---|---|---|---|---|
    | 登録済み | admin | - | `user_not_authorized` | 302 | `root_path` |
    | 登録済み | general | - | `user_not_authorized` | 302 | `root_path` |
    | 未登録 | admin | 成功 | `CompaniesController#create` | 303 | `edit_company_path` |
    | 未登録 | admin | 失敗 | `CompaniesController#create` | 422 | - |
    | 未登録 | general | - | `user_not_authorized` | 302 | `invitation_required_path` |

--- spec/requests/companies_spec.rb
+++ spec/requests/companies_spec.rb
@@ -104,4 +104,89 @@ RSpec.describe "Companies", type: :request do
       end
     end
   end
+
+  describe "POST /companies" do
+    context "ログイン済みの場合" do
+      context "company登録済みの場合" do
+        context "adminユーザーの場合" do
+          let(:user) { create(:user, :admin, :with_company) }
+
+          it "トップページにリダイレクトすること" do
+            sign_in user
+            post companies_path, params: { company: attributes_for(:company) }
+            expect(response).to redirect_to(root_path)
+          end
+        end
+
+        context "generalユーザーの場合" do
+          let(:user) { create(:user, :general, :with_company) }
+
+          it "トップページにリダイレクトすること" do
+            sign_in user
+            post companies_path, params: { company: attributes_for(:company) }
+            expect(response).to redirect_to(root_path)
+          end
+        end
+      end
+
+      context "company未登録の場合" do
+        context "adminユーザーの場合" do
+          let(:user) { create(:user, :admin, company: nil) }
+
+          context "有効なパラメータを送信した場合" do
+            it "ステータスコード303を返すこと" do
+              sign_in user
+              post companies_path, params: { company: attributes_for(:company) }
+              expect(response).to have_http_status(:see_other)
+            end
+
+            it "作成した自社情報がユーザーに紐づくこと" do
+              sign_in user
+              expect {
+                post companies_path, params: { company: attributes_for(:company) }
+              }.to change { user.reload.company_id }.from(nil)
+            end
+
+            it "自社情報の作成・保存に成功すること" do
+              sign_in user
+              expect {
+                post companies_path, params: { company: attributes_for(:company) }
+              }.to change(Company, :count).by(1)
+            end
+
+            it "自社情報編集ページにリダイレクトすること" do
+              sign_in user
+              post companies_path, params: { company: attributes_for(:company) }
+              expect(response).to redirect_to(edit_company_path(user.reload.company))
+            end
+          end
+
+          context "無効なパラメータを送信した場合" do
+            it "ステータスコード422を返すこと" do
+              sign_in user
+              post companies_path, params: { company: attributes_for(:company, :invalid) }
+              expect(response).to have_http_status(:unprocessable_entity)
+            end
+
+            it "自社情報の作成・保存に失敗すること" do
+              sign_in user
+              expect {
+                post companies_path, params: { company: attributes_for(:company, :invalid) }
+              }.not_to change(Company, :count)
+            end
+          end
+        end
+
+        context "generalユーザーの場合" do
+          let(:user) { create(:user, :general, company: nil) }
+
+          it "招待待ちページにリダイレクトすること" do
+            sign_in user
+            post companies_path, params: { company: attributes_for(:company) }
+            expect(response).to redirect_to(invitation_required_path)
+          end
+        end
+      end
+    end
+  end
 end
