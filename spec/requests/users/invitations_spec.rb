d0cc6227aa1c809f3e72e07cd2d23f88f7e76c88
kenyoshizawa <harborseal2521@gmail.com>
2026-06-20 11:00:04 +0000

    test: Users::InvitationsController#newのRequest Spec作成 #53 やったこと： 以下の表に従って、ステータスコードやリダイレクト先が期待通りか検証するRequest Specを作成した。
    ユーザーを招待できるユーザーは（company登録済み・adminユーザー）のみである。
    | company | ユーザー種別 | 実行されるメソッド | ステータスコード | リダイレクト先 |
    |---|---|---|---|---|
    | 登録済み | admin | `Users::InvitationsController#new` | 200 | - |
    | 登録済み | general | `require_admin!` | 302 | `root_path` |
    | 未登録 | admin | `user_not_authorized` | 302 | `new_company_path` |
    | 未登録 | general | `require_admin!` | 302 | `root_path` |

--- spec/requests/users/invitations_spec.rb
+++ spec/requests/users/invitations_spec.rb
@@ -1,7 +1,45 @@
 require "rails_helper"
 
 RSpec.describe "Users::Invitations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "GET /users/invitation/new" do
+    context "送信者がadminユーザー（company登録済み）の場合" do
+      let(:user) { create(:user, :admin, :with_company) }
+
+      it "HTTPステータス200を返すこと" do
+        sign_in user
+        get new_user_invitation_path
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "送信者がgeneralユーザー（company登録済み）の場合" do
+      let(:user) { create(:user, :general, :with_company) }
+
+      it "トップページにリダイレクトすること" do
+        sign_in user
+        get new_user_invitation_path
+        expect(response).to redirect_to(root_path)
+      end
+    end
+
+    context "送信者がadminユーザー（company未登録）の場合" do
+      let(:user) { create(:user, :admin, company: nil) }
+
+      it "自社情報登録ページにリダイレクトすること" do
+        sign_in user
+        get new_user_invitation_path
+        expect(response).to redirect_to(new_company_path)
+      end
+    end
+
+    context "送信者がgeneralユーザー（company未登録）の場合" do
+      let(:user) { create(:user, :general, company: nil) }
+
+      it "トップページにリダイレクトすること" do
+        sign_in user
+        get new_user_invitation_path
+        expect(response).to redirect_to(root_path)
+      end
+    end
   end
 end
